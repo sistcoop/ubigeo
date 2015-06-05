629e625d797b454204bf0affe1cf40b5a8265726
Carlos <carlosthe19916@gmail.com>
2015-06-05 17:50:15 -0500

    192.168.1.50

---
+++ ubigeo-integration/ubigeo-client/src/main/java/org/sistcoop/ubigeo/admin/client/Roles.java
@@ -0,0 +1,5 @@
+package org.sistcoop.ubigeo.admin.client;
+
+public class Roles {
+	public final static String ver_ubigeos = "ver-ubigeos";
+}
--- ubigeo-services/src/main/java/org/sistcoop/ubigeo/services/resources/admin/UbigeoResourceImpl.java
+++ ubigeo-services/src/main/java/org/sistcoop/ubigeo/services/resources/admin/UbigeoResourceImpl.java
@@ -3,24 +3,28 @@ package org.sistcoop.ubigeo.services.resources.admin;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import org.jboss.ejb3.annotation.SecurityDomain;
+import org.sistcoop.ubigeo.admin.client.Roles;
 import org.sistcoop.ubigeo.admin.client.resource.UbigeoResource;
 import org.sistcoop.ubigeo.models.UbigeoModel;
 import org.sistcoop.ubigeo.models.UbigeoProvider;
 import org.sistcoop.ubigeo.models.utils.ModelToRepresentation;
 import org.sistcoop.ubigeo.representations.idm.UbigeoRepresentation;
 
-@Path("/departamentos")
 @Stateless
+@SecurityDomain("keycloak")
 public class UbigeoResourceImpl implements UbigeoResource {
 
 	@Inject
 	protected UbigeoProvider ubigeoProvider;
 	
+	@RolesAllowed(Roles.ver_ubigeos)
+	@Override
 	public List<UbigeoRepresentation> getDepartamentos() {
 		List<UbigeoModel> list = ubigeoProvider.getDepartamentos();
 		List<UbigeoRepresentation> result = new ArrayList<UbigeoRepresentation>();
@@ -30,6 +34,8 @@ public class UbigeoResourceImpl implements UbigeoResource {
 		return result;
 	}
 	
+	@RolesAllowed(Roles.ver_ubigeos)
+	@Override
 	public List<UbigeoRepresentation> getProvincias(@PathParam("codigo") String codigoDepartamento) {
 		List<UbigeoModel> list = ubigeoProvider.getProvincias(codigoDepartamento);
 		List<UbigeoRepresentation> result = new ArrayList<UbigeoRepresentation>();
@@ -39,6 +45,8 @@ public class UbigeoResourceImpl implements UbigeoResource {
 		return result;
 	}
 	
+	@RolesAllowed(Roles.ver_ubigeos)
+	@Override
 	public List<UbigeoRepresentation> getDistritos(@PathParam("codigoDep") String codigoDepartamento, @PathParam("codigoProv") String codigoProvincia) {
 		List<UbigeoModel> list = ubigeoProvider.getDistritos(codigoDepartamento, codigoProvincia);
 		List<UbigeoRepresentation> result = new ArrayList<UbigeoRepresentation>();
